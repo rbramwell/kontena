0777aa7297f28c6a67d11307532cf2f31f983ef2
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-01-03 15:08:18 +0200

    faster omnibus bundle

--- build/travis/omnibus_osx.sh
+++ build/travis/omnibus_osx.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 cd cli/omnibus
+export USE_SYSTEM_GECODE=1
+sudo apt-get install -qq libarchive12 libarchive-dev libgecode-dev graphviz
 bundle install
 bundle exec omnibus build kontena
